26439e145520f0fb493dbfd461b16a958a433f8b
Isaac MM Newton <newton2k1@gmail.com>
2025-02-19 15:11:19 +0100

    exclude temperature.log from repo and improved shell script by introducing /home/chmamai

--- log_temperature.sh
+++ log_temperature.sh
@@ -2,7 +2,7 @@
 # file location: /home/pi/Documents/proxyconf/log_temperature.sh
 # add this line with crontab -e to execute temp logging each minute (remove #)
 #   * *  *   *   *     /home/pi/Documents/proxyconf/log_temperature.sh
-path=/home/pi/Documents/proxyconf
+path=$HOME/Documents/proxyconf
 now=$(date +%Y-%m-%d_%H:%M:%S)
 temp=$(vcgencmd measure_temp)
 rm           $path/actual_CPU_*
